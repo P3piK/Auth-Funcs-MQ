6fc6f7f6cdefba0309f6bb3d8294422ab72e027a
Piotr <grytpiotr@gmail.com>
2022-10-11 13:44:35 +0200

    Implement EmailMessageFactory

---
+++ AuthFuncsMQ.Consumer/Message/EmailMessageBase.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Communication.Email.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthFuncsMQ.Consumer.Message
+{
+    public abstract class EmailMessageBase 
+    {
+        public abstract string Sender { get; }
+        public abstract string Subject { get; }
+        public abstract string Body { get; }
+
+        public EmailMessage GenerateMessage(string recipient)
+        {
+            return new EmailMessage(
+                Sender,
+                new EmailContent(Subject)
+                {
+                    Html = Body
+                },
+                new EmailRecipients(new List<EmailAddress>()
+                {
+                    new EmailAddress(recipient)
+                    {
+                        DisplayName = "no-reply AuthFuncs"
+                    }
+                }));
+        }
+    }
+}
---
+++ AuthFuncsMQ.Consumer/Message/ResetPasswordMessage.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Communication.Email.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthFuncsMQ.Consumer.Message
+{
+    internal class ResetPasswordMessage : EmailMessageBase
+    {
+        public override string Sender => EmailResource.ResetPasswordEmailSender;
+
+        public override string Subject => EmailResource.ResetPasswordEmailSubject;
+
+        public override string Body => EmailResource.ResetPasswordEmailBody;
+    }
+}
